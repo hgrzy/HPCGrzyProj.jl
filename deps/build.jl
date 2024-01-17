85dce52d391b60fccba803fa45f321d375af58e9
hgrzy <hannahg0104@gmail.com>
2024-01-17 18:24:08 -0500

    Dependencies addition

---
+++ deps/build.jl
@@ -0,0 +1,6 @@
+using get_planet_data
+println("Installing NASA JPL Horizons Sun and Planet Data...")
+body_names = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+download_planet_data_from_list(body_names)
+println("NASA JPL Horizons Data downloaded.")
+
