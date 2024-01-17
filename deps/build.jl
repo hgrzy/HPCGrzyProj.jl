e80e9e822e53198ea2acbc808d790f30666344bd
hgrzy <hannahg0104@gmail.com>
2024-01-17 18:34:18 -0500

    deps bug fix

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-using get_planet_data
+include("get_planet_data.jl")
 println("Installing NASA JPL Horizons Sun and Planet Data...")
 body_names = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 download_planet_data_from_list(body_names)
